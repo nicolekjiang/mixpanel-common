ab5dbe31269581e63059795be522f1dce2eab079
Ilya Kamens <ilya@mixpanel.com>
2016-06-07 16:16:16 +0000

    WIP use <content>/slots

--- components/modals/modals.js
+++ components/modals/modals.js
@@ -2,34 +2,27 @@ import WebComponent from 'webcomponent';
 
 import css from './modals.styl';
 
+const template = `
+  <div class="mp-modal">
+    <div class="backdrop"></div>
+    <div class="modal-container">
+      <div class="top-container">
+        <content select="[slot-title]"></content>
+        <content select="[slot-body]"></content>
+      </div>
+      <div class="button-container">
+        <content select="[slot-button]"></content>
+      </div>
+    </div>
+  </div>
+`;
+
 document.registerElement('mp-modal', class MPModal extends WebComponent {
   attachedCallback() {
     this.root = this.createShadowRoot();
-
-    this.main = document.createElement('div');
-    this.main.className = this.className;
-    this.main.classList.add('mp-modal');
-
-    const backdrop = document.createElement('div');
-    backdrop.className = 'backdrop';
-    this.main.appendChild(backdrop);
-
-    const top = document.createElement('div');
-    top.className = 'top-container';
-    top.appendChild(this.getElementsByClassName('mp-modal-title')[0]);
-    top.appendChild(this.getElementsByClassName('mp-modal-subtitle')[0]);
-
-    const modal = document.createElement('div');
-    modal.className = 'modal-container';
-    modal.appendChild(top);
-    modal.appendChild(this.getElementsByTagName('mp-button')[0]);
-    this.main.appendChild(modal);
-
-    this.render();
-  }
-
-  render() {
-    this.root.innerHTML = `<style>${css}</style>`;
-    this.root.appendChild(this.main);
+    this.root.innerHTML = `
+      <style>${css}</style>
+      ${template}
+    `;
   }
 });
