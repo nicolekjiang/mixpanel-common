5394e9feb5d82ee231454af71949a8ff9d75d4c1
Ilya Kamens <ilya@mixpanel.com>
2016-06-07 00:24:44 +0000

    more modal fixes

--- components/buttons/buttons.js
+++ components/buttons/buttons.js
@@ -3,26 +3,33 @@ import WebComponent from 'webcomponent';
 import css from './buttons.styl';
 
 document.registerElement('mp-button', class MPButton extends WebComponent {
-  attachedCallback() {
-    this.root = this.createShadowRoot();
+  createdCallback() {
+    this.createShadowRoot();
     this.main = document.createElement('div');
+    this.attached = false;
+  }
+
+  attachedCallback() {
+    this.attached = true;
     this.main.className = this.className;
     this.main.classList.add('mp-button');
     this.render();
   }
 
   render() {
-    this.root.innerHTML = `<style>${css}</style>`;
-    this.main.innerHTML = `<span class="${this.getAttribute('type')}-text"><content></content></span>`;
+    this.shadowRoot.innerHTML = `<style>${css}</style>`;
+    this.main.innerHTML = `<span class="text"><content></content></span>`;
     if (this.isAttributeEnabled('disabled')) {
       this.main.setAttribute('disabled', true);
     } else {
       this.main.removeAttribute('disabled');
     }
-    this.root.appendChild(this.main);
+    this.shadowRoot.appendChild(this.main);
   }
 
   attributeChangedCallback() {
-    this.render();
+    if (this.attached) {
+      this.render();
+    }
   }
 });
--- components/modals/modals.js
+++ components/modals/modals.js
@@ -5,21 +5,26 @@ import css from './modals.styl';
 document.registerElement('mp-modal', class MPModal extends WebComponent {
   attachedCallback() {
     this.root = this.createShadowRoot();
+
     this.main = document.createElement('div');
     this.main.className = this.className;
     this.main.classList.add('mp-modal');
+
     const backdrop = document.createElement('div');
     backdrop.className = 'backdrop';
-    const modal = document.createElement('div');
-    modal.className = 'modal-container';
+    this.main.appendChild(backdrop);
+
     const top = document.createElement('div');
     top.className = 'top-container';
     top.appendChild(this.getElementsByClassName('mp-modal-title')[0]);
     top.appendChild(this.getElementsByClassName('mp-modal-subtitle')[0]);
+
+    const modal = document.createElement('div');
+    modal.className = 'modal-container';
     modal.appendChild(top);
     modal.appendChild(this.getElementsByTagName('mp-button')[0]);
-    backdrop.appendChild(modal);
-    this.main.appendChild(backdrop);
+    this.main.appendChild(modal);
+
     this.render();
   }
 
