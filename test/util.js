09c3cf60f0b34564f803992c9505c1ef0a35200b
cassie morford <cassie.morford@mixpanel.com>
2016-06-21 18:50:03 +0000

    tests

--- test/util.js
+++ test/util.js
@@ -10,4 +10,12 @@ describe('pluralize()', function() {
   it('does not pluralize singulars', function() {
     expect(pluralize('wombat', 1)).to.equal('wombat');
   });
+
+  it('pluralizes 0 of something', function() {
+    expect(pluralize('helicopter', 0)).to.equal('helicopters');
+  });
+
+  it('uses third argument to pluralize', function() {
+    expect(pluralize('die', 4, 'dice')).to.equal('dice');
+  });
 });
