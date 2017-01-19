8f9ade59bc573c15b2d4c77176e686e8c6711aaa
Jonathan Lack <jonathan.lack@mixpanel.com>
2017-01-19 11:02:44 -0800

    sentence-casing tags

--- examples/style-guide/index.js
+++ examples/style-guide/index.js
@@ -34,8 +34,8 @@ document.registerElement('style-guide', class extends Component {
         selectedBookmarkId: null,
         tagSelectorLoadState: 'idle',
         tagSelectorData: {
-          selectedTags: new Set(["My tag", "Another tag"]),
-          allTags: new Set(["My tag", "Another tag", "Our tag", "His tag"]),
+          selectedTags: new Set(["my tag", "another tag"]),
+          allTags: new Set(["my tag", "another tag", "Our tag", "His tag"]),
         }
       },
       helpers: {
