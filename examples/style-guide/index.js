2f17e31194bfd64916880f81a6f8377cc9cc057c
Jonathan Lack <jonathan.lack@mixpanel.com>
2017-01-20 17:08:41 -0800

    add pinned tags that stay on top of list (use case is “All Reports” tag in dashboards)

--- examples/style-guide/index.js
+++ examples/style-guide/index.js
@@ -12,11 +12,11 @@ import bookmarks from './bookmark-data.json';
 import template from './index.jade';
 import './index.styl';
 
-document.registerElement('style-guide', class extends Component {
+document.registerElement(`style-guide`, class extends Component {
   get config() {
     return {
       defaultState: {
-        blueToggleValue: 'option1',
+        blueToggleValue: `option1`,
         bookmarks,
         COLORS,
         SVG_ICONS,
@@ -32,11 +32,12 @@ document.registerElement('style-guide', class extends Component {
         },
         savingBookmark: false,
         selectedBookmarkId: null,
-        tagSelectorLoadState: 'idle',
+        tagSelectorLoadState: `idle`,
         tagSelectorData: {
-          selectedTags: new Set(["my tag", "another tag"]),
-          allTags: new Set(["my tag", "another tag", "our tag", "his tag"]),
-        }
+          pinnedTags: new Set([`pinned tag`]),
+          selectedTags: new Set([`my tag`, `another tag`]),
+          allTags: new Set([`my tag`, `another tag`, `pinned tag`, `our tag`, `his tag`]),
+        },
       },
       helpers: {
         blueToggleChanged: ev => this.update({blueToggleValue: ev.detail.selected}),
@@ -49,17 +50,17 @@ document.registerElement('style-guide', class extends Component {
           this.update();
         },
         handleModalChange: (key, state) => {
-          this.state.open[key] = state === 'open';
+          this.state.open[key] = state === `open`;
           this.update();
         },
         handleNamerChange: e => {
-          console.log('mp-input-group value changed to: ', e.target.value);
+          console.log(`mp-input-group value changed to: `, e.target.value);
         },
         handleNamerSubmit: () => {
           this.update({inputGroupSaving: true});
           setTimeout(() => {
             this.update({inputGroupSaving: false});
-            alert('Saved!');
+            alert(`Saved!`);
           }, 2000);
         },
         toggleMenu: () => {
@@ -68,21 +69,21 @@ document.registerElement('style-guide', class extends Component {
         },
         handleBookmarksMenuSubmit: e => {
           switch (e.detail.action) {
-            case 'select':
-              console.log('Selected:', e.detail.value.id);
+            case `select`:
+              console.log(`Selected:`, e.detail.value.id);
               break;
-            case 'delete':
+            case `delete`:
               this.update({bookmarks: this.state.bookmarks.filter(b => b.id !== e.detail.bookmarkId)});
               break;
-            case 'create':
+            case `create`:
               this.update({savingBookmark: true});
               setTimeout(() => {
-                console.log('Created:', e.detail.name);
+                console.log(`Created:`, e.detail.name);
                 const newBookmark = {
                   id: (new Date()).getTime(),
                   user_id: 1,
                   name: e.detail.name,
-                  user: 'John D.',
+                  user: `John D.`,
                 };
                 this.state.bookmarks.push(newBookmark);
                 this.update({savingBookmark: false, selectedBookmarkId: newBookmark.id});
@@ -100,7 +101,7 @@ document.registerElement('style-guide', class extends Component {
           this.update();
         },
         handleTagSelectorDropMenuChange: e => {
-          this.state.open.tagSelector = e.detail.state !== 'closed';
+          this.state.open.tagSelector = e.detail.state !== `closed`;
           this.update();
         },
         handleTagSelectorChange: e => {
@@ -108,18 +109,18 @@ document.registerElement('style-guide', class extends Component {
           // handle whatever business logic we need to deal with when
           // adding or removing a tag
           const tagName = e.detail.tagName;
-          if (e.detail.action === 'addTag') {
+          if (e.detail.action === `addTag`) {
             this.state.tagSelectorData.selectedTags.add(tagName);
             this.state.tagSelectorData.allTags.add(tagName);
-          } else if (e.detail.action === 'removeTag') {
+          } else if (e.detail.action === `removeTag`) {
             this.state.tagSelectorData.selectedTags.delete(tagName);
           }
           // normally we'd persist to server and this timeout represents that
           // async call
           clearTimeout(this.loadingTagTimeout);
-          this.update({tagSelectorLoadState: 'loading_tag'});
+          this.update({tagSelectorLoadState: `loading_tag`});
           this.loadingTagTimeout = setTimeout(() => {
-            this.update({tagSelectorLoadState: 'loaded_tag'});
+            this.update({tagSelectorLoadState: `loaded_tag`});
           }, 700);
         },
         handleTagSelectorSubmit: e => {
