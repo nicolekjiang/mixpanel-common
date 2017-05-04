eee26fb5f1a4f0ca16abc35f51c3f2fa6e252e37
Ricky Doar <ricky@mixpanel.com>
2017-05-03 23:35:33 -0700

    1x vs 2x icons

--- examples/style-guide-new/index.js
+++ examples/style-guide-new/index.js
@@ -25,7 +25,7 @@ document.registerElement('style-guide', class extends Component {
       defaultState: {
         COLORS,
         SVG_ICONS,
-        sectionOpen: 'widgets',
+        sectionOpen: 'iconography',
         subSectionOpen: null,
         sectionFilter: null,
         open: {
