65e1edc94cac3b9d5158f067ce2e025c144139ad
Ricky Doar <ricky@mixpanel.com>
2017-05-10 14:31:20 -0700

    refined search

--- examples/style-guide-new/components/typography/index.js
+++ examples/style-guide-new/components/typography/index.js
@@ -6,15 +6,6 @@ import './index.styl';
 document.registerElement('typography-section', class extends Component {
   get config() {
     return {
-      helpers: {
-        hideSectionOnSearch: sectionId => {
-          const sectionKeywords = sectionId.split('-').join(' ');
-          if (this.state.sectionOpen != 'search' || sectionKeywords.includes(this.state.searchTerm)) {
-            return false
-          }
-          return true;
-        },
-      },
       template,
     };
   }
